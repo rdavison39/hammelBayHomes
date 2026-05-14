1ea27d463d0157e26b86fe1ef74a419821681a36
Ron Davison <ronaldjdavison@hotmail.com>
2026-05-14 00:56:35 -0400

    add compass Star Celebrations site

--- create new website.bat
+++ create new website.bat
@@ -1,5 +1,6 @@
-REM run this bat file when youy have added a new website (also setup a bucket in Amazon S3)
+REM run this bat file when youy have added a new website (also setup a bucket in Amazon S3) - 
+REM - what is in quotes is just a comment
 
 git add .
-git commit -m "add xxx site"
+git commit -m "add compass Star Celebrations site"
 git push
